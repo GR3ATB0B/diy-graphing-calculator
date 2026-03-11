c189aa312420d2034b948041ba897a59455e24f2
Nash Vogeltanz <71197497+GR3ATB0B@users.noreply.github.com>
2026-03-10 23:30:35 -0400

    refactor: rewrite firmware for ILI9341 + Adafruit_GFX (replaces TFT_eSPI/ST7789)

--- firmware/src/screen.cpp
+++ firmware/src/screen.cpp
@@ -2,15 +2,11 @@
 #include "screen.h"
 
 void ScreenManager::push(Screen* s) {
-    if (_top < MAX_SCREENS - 1) {
-        _stack[++_top] = s;
-    }
+    if (_top < MAX_SCREENS - 1) _stack[++_top] = s;
 }
 
 void ScreenManager::pop() {
-    if (_top >= 0) {
-        _top--;
-    }
+    if (_top >= 0) _top--;
 }
 
 Screen* ScreenManager::current() {
